5077fc3f2b42a6e1f3d808af3b0922024d3fddd6
LamiIIIa <laminoji@gmail.com>
2025-10-22 22:38:32 +0300

    added flag -f

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,7 @@ const program = new Command();
 program
   .name("gendiff")
   .description("Compares two configuration files and shows a difference.")
-  .version("0.0.1");
+  .version("0.0.1")
+  .option("-f, -format [type]", "output format");
 
 program.parse(process.argv);
