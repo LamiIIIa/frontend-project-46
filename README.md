f6990029945dee85db9aec16e5cdc8d84f226a68
LamiIIIa <laminoji@gmail.com>
2025-11-01 13:35:45 +0300

    step 4

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/LamiIIIa/frontend-project-46/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/LamiIIIa/frontend-project-46/actions)
 [![SonarQube Cloud](https://sonarcloud.io/images/project_badges/sonarcloud-light.svg)](https://sonarcloud.io/summary/new_code?id=LamiIIIa_frontend-project-46)
+[![asciicast](https://asciinema.org/a/CPIYxstjgNAnalVST11IBnjUv.svg)](https://asciinema.org/a/CPIYxstjgNAnalVST11IBnjUv)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
+import _ from "lodash";
 import parse from "./parsers.js";
 
 export default (fp1, fp2) => {
-const f1 = parse(fp1);
-const f2 = parse(fp2);
-return {f1, f2}
+  const f1 = parse(fp1);
+  const f2 = parse(fp2);
+
+  const keys = _.sortBy([...new Set([...Object.keys(f1), ...Object.keys(f2)])]);
+
+  const lines = keys.flatMap((key) => {
+    const has1 = key in f1;
+    const has2 = key in f2;
+    if (has1 && has2) {
+      if (_.isEqual(f1[key], f2[key])) {
+        return `  ${key}: ${f1[key]}`;
+      }
+      return [`- ${key}: ${f1[key]}`, `+ ${key}: ${f2[key]}`];
+    }
+    if (has1) return `- ${key}: ${f1[key]}`;
+    if (has2) return `+ ${key}: ${f2[key]}`;
+    return [];
+  });
+
+  return ["{", ...lines, "}"].join("\n");
 };
