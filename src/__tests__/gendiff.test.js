a38398705439f1ea8d585c3b797f675a81f63b4a
LamiIIIa <laminoji@gmail.com>
2025-11-05 22:16:19 +0300

    new tests

--- src/__tests__/gendiff.test.js
+++ src/__tests__/gendiff.test.js
@@ -5,32 +5,106 @@ import genDiff from "../index.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-test("gendiff works for flat JSON", () => {
-  const fp1 = path.join(__dirname, "__fixtures__", "file1.json");
-  const fp2 = path.join(__dirname, "__fixtures__", "file2.json");
-  const diff = genDiff(fp1, fp2);
+describe("gendiff for nested structures", () => {
+  test("works for nested JSON", () => {
+    const fp1 = path.join(__dirname, "__fixtures__", "file1.json");
+    const fp2 = path.join(__dirname, "__fixtures__", "file2.json");
+    const diff = genDiff(fp1, fp2);
 
-  expect(diff).toBe(`{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
+    expect(diff).toBe(`{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow:
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
 }`);
-});
+  });
 
-test("gendiff works for flat YAML", () => {
-  const f1 = path.join(__dirname, "__fixtures__", "file1.yml");
-  const f2 = path.join(__dirname, "__fixtures__", "file2.yml");
-  const diff = genDiff(f1, f2);
+  test("works for nested YAML", () => {
+    const fp1 = path.join(__dirname, "__fixtures__", "file1.yml");
+    const fp2 = path.join(__dirname, "__fixtures__", "file2.yml");
+    const diff = genDiff(fp1, fp2);
 
-  expect(diff).toBe(`{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
+    expect(diff).toBe(`{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow:
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
 }`);
+  });
 });
