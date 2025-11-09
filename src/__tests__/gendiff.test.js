f2affb8c4505d68f0eebbb66118eadb806ba1f92
LamiIIIa <laminoji@gmail.com>
2025-11-09 18:26:54 +0300

    new test

--- src/__tests__/gendiff.test.js
+++ src/__tests__/gendiff.test.js
@@ -108,6 +108,28 @@ describe("gendiff for nested structures", () => {
 }`);
   });
 
+  test("works for plain format (JSON)", () => {
+    const fp1 = path.join(__dirname, "__fixtures__", "file1.json");
+    const fp2 = path.join(__dirname, "__fixtures__", "file2.json");
+
+    const expected = [
+      "Property 'common.follow' was added with value: false",
+      "Property 'common.setting2' was removed",
+      "Property 'common.setting3' was updated. From true to null",
+      "Property 'common.setting4' was added with value: 'blah blah'",
+      "Property 'common.setting5' was added with value: [complex value]",
+      "Property 'common.setting6.doge.wow' was updated. From '' to 'so much'",
+      "Property 'common.setting6.ops' was added with value: 'vops'",
+      "Property 'group1.baz' was updated. From 'bas' to 'bars'",
+      "Property 'group1.nest' was updated. From [complex value] to 'str'",
+      "Property 'group2' was removed",
+      "Property 'group3' was added with value: [complex value]",
+    ].join("\n");
+
+    const diff = genDiff(fp1, fp2, "plain");
+    expect(diff).toBe(expected);
+  });
+
   test("works for plain format (YAML)", () => {
     const fp1 = path.join(__dirname, "__fixtures__", "file1.yml");
     const fp2 = path.join(__dirname, "__fixtures__", "file2.yml");
